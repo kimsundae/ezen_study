e51024b63397b1019c4493e328bb5b5644390d14
504 <504@DESKTOP-3IJG99V>
2023-06-19 18:00:18 +0900

    06 19

---
+++ frontend/public/js/1_기초문법/3_연산자.js
@@ -0,0 +1,38 @@
+
+
+//1. 산술연산자
+console.log('더하기 : ' + 10+3)	// 103
+console.log('더하기 : ' + (10+3)) // 13
+console.log('빼기 : ' + (10-3))   //7
+console.log('곱하기 : '+ (10*3))	// 30
+console.log('나누기 : ' + (10/3)) // 3.333333~
+console.log('나머지 : ' + (10%3))  // 1
+
+//문제 1
+let 국어 = prompt('국어 점수 : ')
+let 영어 = prompt('영어 점수 : ')
+let 수학 = prompt('수학 점수 : ')
+
+let 총점 = 국어 + 영어 + 수학
+let 평균 = 총점 / 3
+console.log('문제 1')
+console.log('총점 : ' + 총점)
+console.log('평균 : ' + (총점 / 3))
+
+// 문제 2
+console.log('문제 2')
+//1. 반지름 입력받기
+let 반지름 = prompt() 
+
+//2. 연산 결과 출력
+console.log('원 넓이 = ' + (반지름 * 반지름 * 3.14))
+
+// 문제 3
+let 앞실수 = prompt()
+let 뒤실수 = prompt()
+consol.log('문제 3')
+console.log
+
+
+
+
