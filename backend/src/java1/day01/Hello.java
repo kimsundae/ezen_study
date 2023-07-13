4c9a6cd7f8a1c652bad5ebd672594f00a1def96c
504 <504@DESKTOP-J4S8LEN>
2023-07-13 16:13:06 +0900

    0713 16:12 day26 JAVA변수와 타입

---
+++ backend/src/java1/day01/Hello.java
@@ -0,0 +1,22 @@
+// 한줄 주석
+/* 여러줄 주석 */
+
+package java1.day01; // 패키지/폴더 경로
+
+public class Hello { // class start // 클래스 선언 // 'Hello' 클래스명 = 숫자시작
+	// 1. 모든 코드는 클래스 안에서 작성
+	// 2. 모든 실행코드는 main 함수 안에서 작성 [ 스레드 = 코드읽은흐름 ] 포함
+	
+	// { } 마다 tap 한번 적용
+
+	public static void main(String[] args) {//m start / 메소드/함수
+		// console.log() vs System.out.println();
+		// syso + ctrl spacebar
+		System.out.println("Hello Java");
+		// 실행 : ctrl + F11 , 해당 오른쪽클릭 -> RUN , 상단메뉴 RUN
+	} // m end 
+	
+	//실행 코드는 main 함수 밖에서 작성X
+	//System.out.println("Hello Java");
+
+}// class end   
--- frontendProject/src/main/webapp/JS/kiosk.js
+++ frontendProject/src/main/webapp/JS/kiosk.js
@@ -6,7 +6,7 @@ console.log('kiosk.js 열림');
 // 1. 광고이미지 변경
 	// 1. 광고이미지 여러개 저장하고 있는 공간 = 배열
 let bimgList = [ 'himg1.png' , 'himg2.png' , 'himg3.jpg' , 'himg4.png' , 'himg5.png' ]
-
+	
 //1. 카테고리 여러개 저장하는 배열
 let categoryList = [ '신제품(new)', '프리미엄' , '와퍼&주니어' , '치킨&슈림프버거' , '올데이킹&킹모닝' ];
 	//1. 제품 여러개 저장하는 배열
