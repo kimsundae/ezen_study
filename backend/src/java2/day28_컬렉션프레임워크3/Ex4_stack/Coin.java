99cc9bf282e04fd0ca4610cc1f284d183267839f
kimgeunbae <naa159@naver.com>
2023-08-29 12:33:27 +0900

    컬렉션 프레임워크 pop , stack

---
+++ backend/src/java2/day28_컬렉션프레임워크3/Ex4_stack/Coin.java
@@ -0,0 +1,22 @@
+package java2.day28_컬렉션프레임워크3.Ex4_stack;
+
+public class Coin {
+
+	// 필드
+	private int value;
+	//생성자
+	public Coin(int value) {this.value = value;}
+	// 메소드 get, set
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	@Override
+	public String toString() {
+		return "Coin [value=" + value + "]";
+	}
+	
+	
+}
