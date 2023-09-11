2a0d02a6d3a2037738689fe207c657c5efaa0fc0
kimgeunbae <naa159@naver.com>
2023-09-11 11:21:01 +0900

    Chatting js 수정

--- jspweb/src/main/webapp/chatting/chatting.js
+++ jspweb/src/main/webapp/chatting/chatting.js
@@ -1,24 +1,33 @@
+if(loginState == false){alert('회원전용 페이지입니다.'); location.href = "/jspweb/member/login.jsp";}
 
 // 1. 클라이언트 소켓 만들기
+let 클라이언트소켓 = new WebSocket("ws://localhost:8080/jspweb/ChattingSocket")
+//-------------------------------------------------------//
 
-	// 1. JS웹 소켓 만들기 [WebSocket클래스]
-		// new WebSocket('서버소켓 URL'); 
-	let 클라이언트소켓 = new WebSocket('ws://localhost:8080/jspweb/ChattingSocket'); console.log(클라이언트소켓)
+/*
+	JS[ HTML파일 종속된 파일 - HTML 안에서 실행되는 구조 ]
+
+	* 
+		클라이언트 : 사용 데이터 요청 하는 PC
+		서버 : 데이터 제공하는 PC
+
+	* 소켓이란.
+		- 통신의 종착점[데이터가 전달되는 위치 -받는사람 -보낸사람]
 	
-	// 내가 만든 함수를 클라이언트소켓 속성에 대입
-	클라이언트소켓.onmessage = ( event ) => onmsg(event);
+		-예시
+		[클라이언트소켓]							[서버소켓]
+		강호동집								 우체국
+			- 안산
+		
+		신동엽집
+			- 수원	
+		
+		
+		
+	* WebSocket 웹소켓 라이브러리
+		- 소켓 관련된 함수들을 제공하는 클래스
+		
+
+
+*/
 	
-function msgsend(){
-	
-	let msg = document.querySelector('.inputcontent').value;
-	// 2. 클라이언트소켓 .send()
-	클라이언트소켓.send(msg); 
-}
-// 연결된 서버소켓으로부터 메세지를 받았을 때
-function onmsg(e){
-	console.log(e)
-	console.log(e.data);
-	
-	document.querySelector('.contentbox')
-			.innerHTML += `<div>${e.data}</div>`
-}
