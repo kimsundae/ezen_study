28b82e39916e5f6f358474cab2acb96ae3c62d85
kimgeunbae <naa159@naver.com>
2023-09-22 11:22:15 +0900

    포인트 결제 기능, 포인트 데이터베이스 저장 기능

--- jspweb/src/main/webapp/js/payment/payment.js
+++ jspweb/src/main/webapp/js/payment/payment.js
@@ -1,5 +1,4 @@
-// 1. 테스트
-setPoint( 1000, '회원가입축하')
+
 // 1. 포인트 사용 함수
 function setPoint( mpamount , mpcomment ){
 	if(loginState == false ){return;}
@@ -7,6 +6,7 @@ function setPoint( mpamount , mpcomment ){
 		url:"/jspweb/PointController",
 		method:"post",
 		data : {mpamount:mpamount , mpcomment:mpcomment},
+		async : false , /* ajax가 응답이 올 때까지 대기상태 = 동기식 */
 		success : r=>{console.log(r)},
 		error : e=>{console.log(e)}
 	})
@@ -14,11 +14,14 @@ function setPoint( mpamount , mpcomment ){
 //2. 내 포인트 확인 함수
 function getPoint(){
 	if( loginState == false ) {return;}
+	
+	
 	$.ajax({
 		url:"/jspweb/PointController",
 		method:"get",
 		data : {type: "getPoint", mpamount:mpamount , mpcomment:mpcomment},
-		success : r=>{console.log(r)},
+		success : r=>{console.log(r)		
+		},
 		error : e=>{console.log(e)}
 	})	
 }
@@ -40,20 +43,48 @@ function getPointList(){
 //--------------- 결제 연동-------------------- //
 // 1. 아임포트 
 IMP.init('imp76071486') // 예: 'imp00000000a' 아임포트 콘솔에서 확인
-// 2.
+// 2. 결제 함수[속성은 pg사마다 상이]
   function requestPay() {
+	  
+	  // 2-1 : 만약에 결제방식을 선택했으면
+	  if( pay_method == '' )return alert('결제방식을 선택해주세요')
+	  
+	  // 2-2 : 주문번호 만들기
+	  	// 고유성/중복x/PK/식별키
+	  	// 1. 날짜(밀리초)+상품코드+회원번호 조합해서 설계
+	  	// 2. 회사 전화 번호
+	  	let now = new Date(); console.log(now); // 현재날짜
+	  	let time = now.getTime();console.log(time); // 현재 시간 밀리초
+	  	let merchant_uid =  "p"+time +"-"+ loginMid; console.log(merchant_uid)
     IMP.request_pay({
-      pg: "html5_inicis.INIBillTst",
-      pay_method: "card",
+      pg: "html5_inicis.INIBillTst",	// 아임포트 콘솔에서 확인
+      pay_method: pay_method,
       merchant_uid: "ORD20180131-0000011",   // 주문번호
-      name: "노르웨이 회전 의자",
-      amount: 64900,                         // 숫자 타입
+      name: "포인트 결제",	
+      amount: amount,                         // 숫자 타입
       buyer_email: "gildong@gmail.com",
       buyer_name: "홍길동",
       buyer_tel: "010-4242-4242",
       buyer_addr: "서울특별시 강남구 신사동",
       buyer_postcode: "01181"
     }, function (rsp) { // callback
+   		console.log(rsp)
       //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
+      if( rsp.success ){
+		  
+	  }else{ // 결제 실패 [테스트 : 실패가 성공이라는 가정 ]
+		 alert('.결제성공') 
+		 // 1. 포인트 적립 기능처리
+		 setPoint( amount , '포인트결제');
+		 // 2. 결제 내역 테이블 기능처리[구현x]
+	  }
     });
   }
+  
+// 3. 결제 방식 선택 함수
+let pay_method = ''; // 결제방식
+function onPayMethod( method ){
+	pay_method = method;
+}
+let amount = 0; // 결제금액
+function onAmount( value ){ amount = value; }
--- jspweb/src/main/webapp/member/payment.jsp
+++ jspweb/src/main/webapp/member/payment.jsp
@@ -9,11 +9,24 @@
 <body>
 <%@include file="../header.jsp" %>
 	
-	<div class="webcontainer"><!--회원가입 전체 구역  -->
-		<button onclick="requestPay()" type="button"> 결제하기 </button>
+	<div class="webcontainer"></div>
+		
+		<div>
+			<button onclick="onAmount(1000)" type="button">1000포인트</button>
+			<button onclick="onAmount(5000)" type="button">5000포인트</button>
+			<button onclick="onAmount(10000)" type="button">10000포인트</button>
+			<button onclick="onAmount(50000)" type="button">50000포인트</button>
+			<button onclick="onAmount(100000)" type="button">100000포인트</button>
+		</div>
+	
+	<div >
+		<button onclick="onPayMethod( 'card' )" type="button">신용카드</button>
+		<button onclick="onPayMethod( 'trans' )" type="button">실시간 계좌이체</button>
+		<button onclick="onPayMethod( 'vbank' )" type="button">가상계좌</button>
+		<button onclick="onPayMethod( 'phone' )" type="button">휴대폰소액결제</button>
+		<!-- HTML에서 매개변수 전달시 문자타입일 경우 ''문자 처리 -->
 	</div>
-	
-	
+	<button onclick="requestPay()" type="button">결제하기</button>
 	<script src="https://cdn.iamport.kr/v1/iamport.js"></script>
 	<script src="/jspweb/js/payment/payment.js" type="text/javascript"></script>			
 </body>	
